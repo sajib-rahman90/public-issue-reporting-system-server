bb688034fe65b364261383121565d1238bc20502
sajib-rahman90 <sajibrahman0090@gmail.com>
2026-05-11 00:00:27 +0600

    created Issues api and setup Mongodb in my server site

--- index.js
+++ index.js
@@ -1,12 +1,60 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hihlt50.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("public-issue-report");
+    const usersCollection = db.collection("users");
+    const issuesCollection = db.collection("issues");
+
+    app.get("/all-issues", async (req, res) => {
+      const query = {};
+      const result = await issuesCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/issues", async (req, res) => {
+      const data = req.body;
+      const result = await issuesCollection.insertOne(data);
+
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Public Issue Reporting system server sit is running");
