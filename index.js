30963deda279ff166e0d59b844d16aa4d7c3feb6
sajib-rahman90 <sajibrahman0090@gmail.com>
2026-05-12 00:00:04 +0600

    create delete api for delete an issue from issues details page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -31,15 +31,44 @@ async function run() {
     const usersCollection = db.collection("users");
     const issuesCollection = db.collection("issues");
 
-    app.get("/all-issues", async (req, res) => {
+    app.get("/issues", async (req, res) => {
       const query = {};
       const result = await issuesCollection.find(query).toArray();
       res.send(result);
     });
+    app.get("/issues/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+
+        const result = await issuesCollection.findOne({
+          _id: new ObjectId(id),
+        });
+
+        if (!result) {
+          return res.status(404).send({ message: "Issue not found" });
+        }
+
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: "Server error" });
+      }
+    });
 
     app.post("/issues", async (req, res) => {
-      const data = req.body;
-      const result = await issuesCollection.insertOne(data);
+      const issueInfo = req.body;
+      const result = await issuesCollection.insertOne(issueInfo);
+
+      res.send(result);
+    });
+
+    //create delete api for delete an issue from issues details page
+    app.delete("/issues/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+
+      const result = await issuesCollection.deleteOne(query);
 
       res.send(result);
     });
